583053453617eb37c93630686e52ebdaa11d6eb3
jcbebr <jcbebr@gmail.com>
2018-11-20 19:57:46 -0200

    Atualizando nomenclaturas

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -22,7 +22,7 @@ public class MainFrame extends JFrame {
     private Container contentPane;
     private JMenuBar mainMenu;
 
-    private JPanel[] jps;
+    private JPanel[] menuPanels;
 
     /**
      * Construtor da classe Frame Sistema.
@@ -48,7 +48,7 @@ public class MainFrame extends JFrame {
         //Instancia o painel princpal da aplicação, onde as telas internas serão exibidas
         mainPanel = new JPanel(null);
         mainPanel.setSize(mainDimension);
-        mainPanel.setBackground(Color.GREEN);
+        //mainPanel.setBackground(Color.GREEN);
 
         //Pega o painel da JFrame
         contentPane = super.getContentPane();
@@ -59,53 +59,35 @@ public class MainFrame extends JFrame {
         mainMenu = new MainMenu(this);
         super.setJMenuBar(mainMenu);
 
-        jps = new JPanel[4];
-
-        jps[0] = new RegisterTeacherPanel(this);
-        //jps[0].setBackground(Color.red);
-        mainPanel.add(jps[0]);
-        jps[0].setSize(mainDimension);
-        jps[0].setLocation(0, 0);
-        jps[0].setVisible(false);
-        jps[0].setEnabled(false);
-
-        jps[1] = new JPanel();
-        jps[1].setBackground(Color.ORANGE);
-        mainPanel.add(jps[1]);
-        jps[1].setSize(mainDimension);
-        jps[1].setLocation(0, 0);
-        jps[1].setVisible(false);
-        jps[1].setEnabled(false);
-
-        jps[2] = new JPanel();
-        jps[2].setBackground(Color.DARK_GRAY);
-        mainPanel.add(jps[2]);
-        jps[2].setSize(mainDimension);
-        jps[2].setLocation(0, 0);
-        jps[2].setVisible(false);
-        jps[2].setEnabled(false);
-
-        jps[3] = new JPanel();
-        jps[3].setBackground(Color.BLACK);
-        mainPanel.add(jps[3]);
-        jps[3].setSize(mainDimension);
-        jps[3].setLocation(0, 0);
-        jps[3].setVisible(false);
-        jps[3].setEnabled(false);
-
+        menuPanels = new JPanel[4];
+        initMenuPanel(new RegisterTeacherPanel(this), 0);
+        initMenuPanel(new JPanel(), 1);
+        initMenuPanel(new JPanel(), 2);
+        initMenuPanel(new JPanel(), 3);
+        
+    }
+    
+    private void initMenuPanel(JPanel jpanel, int i){
+        menuPanels[i] = jpanel;
+        //jps[i].setBackground(Color.BLACK);
+        mainPanel.add(menuPanels[i]);
+        menuPanels[i].setSize(mainDimension);
+        menuPanels[i].setLocation(0, 0);
+        menuPanels[i].setVisible(false);
+        menuPanels[i].setEnabled(false);
     }
 
     public void goToMainScreen() {
-        for (int j = 0; j < jps.length; j++) {
-            jps[j].setVisible(false);
-            jps[j].setEnabled(false);
+        for (int j = 0; j < menuPanels.length; j++) {
+            menuPanels[j].setVisible(false);
+            menuPanels[j].setEnabled(false);
         }
     }
 
     public void changeJPanel(int i) {
         goToMainScreen();
-        jps[i].setVisible(true);
-        jps[i].setEnabled(true);
+        menuPanels[i].setVisible(true);
+        menuPanels[i].setEnabled(true);
     }
 
 }
