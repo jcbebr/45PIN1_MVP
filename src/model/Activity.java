8879626790411f262b9bbe815643554653dd7c8c
jcbebr <jcbebr@gmail.com>
2018-11-21 17:11:01 -0200

    Resolvendo merge coflict

---
+++ src/model/Activity.java
@@ -0,0 +1,38 @@
+package model;
+
+/**
+ *
+ * @author José Carlos
+ */
+public class Activity {
+    
+    private String name;
+    private String type;
+    private String hours;
+
+    public Activity(String name, String type, String hours) {
+        this.name = name;
+        this.type = type;
+        this.hours = hours;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+    @Override
+    public String toString() {
+        return "Activitie{" + "name=" + name + ", type=" + type + ", hours=" + hours + '}';
+    }
+    
+    
+    
+}
